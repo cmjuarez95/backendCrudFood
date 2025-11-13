6811cccf851c3d44fbf7c64ecb95e23d11bf1d55
Mariano Juarez <cm.juarez.95@gmail.com>
2025-11-12 21:31:24 -0300

    cambie el generar JWT

--- src/middleware/generarJWT.js
+++ src/middleware/generarJWT.js
@@ -1,14 +1,20 @@
 import jwt from "jsonwebtoken";
 
-const generarJWT = (usuario, email)=>{
-    try {
-        const payload={usuario,email}
-         const token = jwt.sign(payload, process.env.SECRETJWT,{expiresIn:"2h"});
-         return token;
-    } catch (error) {
-        console.error(error);
-        throw new Error("Error al generar token");
+const generarJWT = (usuario, email) => {
+  try {
+    console.log("🔹 SECRETJWT:", process.env.SECRETJWT);
+    const payload = { usuario, email };
+
+    if (!process.env.SECRETJWT) {
+      throw new Error("⚠️ SECRETJWT no está definida en el entorno");
     }
+
+    const token = jwt.sign(payload, process.env.SECRETJWT, { expiresIn: "2h" });
+    return token;
+  } catch (error) {
+    console.error("Error en generarJWT:", error.message);
+    throw new Error("Error al generar token");
+  }
 };
 
-export default generarJWT
+export default generarJWT;
